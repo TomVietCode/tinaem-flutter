58d1ca3569a75bd87e2fdabe430837db63fe0706
TomVietCode <mcuong04.work@gmail.com>
2025-02-16 22:23:20 +0700

    Init project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text("Kiểm tra Firebase")),
+        body: Center(
+          child: Text("🔥 Firebase đã kết nối thành công!"),
+        ),
+      ),
+    );
+  }
+}
